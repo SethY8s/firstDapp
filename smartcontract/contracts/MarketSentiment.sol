0f228c614e86afc0cc19533a6f13263181e73d17
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-10 20:39:34 -0400

    added changes for pushing to testnet ether

--- smartcontract/contracts/MarketSentiment.sol
+++ smartcontract/contracts/MarketSentiment.sol
@@ -63,6 +63,7 @@ contract MarketSentiment {
         return (t.up, t.down);
     }
 
+// this function doesnt use any gas since it is a 'view' function
     function getArr(uint256 num) public view returns(string memory) {
         console.log('test log');
         return tickerArray[num];
--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,8 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 require('@nomiclabs/hardhat-etherscan');
-const dotenv = require("dotenv")
+const dotenv = require('dotenv');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  newworks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env,
+  },
 };
