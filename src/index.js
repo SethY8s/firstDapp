8f328b5dd7068a0ba4b3b4cf69dd99cf570092b2
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-17 18:39:27 -0400

    added moralis and moralis-react

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { MoralisProvider } from 'react-moralis';
+import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <MoralisProvider appId="" severUrl=""> */}
+    <MoralisProvider
+      appId={process.env.REACT_APP_MORALIS_ID}
+      serverUrl={process.env.REACT_APP_MORALIS_URL}
+    >
       <App />
-    {/* </MoralisProvider> */}
+    </MoralisProvider>
   </React.StrictMode>
 );
