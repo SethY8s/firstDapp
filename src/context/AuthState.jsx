0d6adb4160d99f1010d2d4a8d7237f449c599b01
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-19 19:05:07 -0400

    Addd more global state

--- src/context/AuthState.jsx
+++ src/context/AuthState.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import AuthContext from "./authContext";
+import React, { useState } from 'react';
+import AuthContext from './authContext';
 
 export default function AuthState(props) {
+  const [web3Provider, setWeb3Provider] = useState(null);
+
   const hello = 'hello this is from the context';
   const value = {
-    hello: hello,
+    hello,
+    web3Provider,
+    setWeb3Provider,
   };
 
   return (
-    <AuthContext.Provider value={value}>
-      {props.children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
   );
 }
