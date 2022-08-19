ddc61063c98be921a8c7f07388fbb3ce957fad32
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-18 21:50:07 -0400

    added formatting to donate and added css

--- src/components/donate/Donate.jsx
+++ src/components/donate/Donate.jsx
@@ -3,10 +3,13 @@ import './donate.css';
 
 export default function Donate() {
   return (
-    <div className='donate-container'>
+    <div className="donate-container">
       <div className="donate">
         <h4>If you enjoyoed my first Dapp, feel free to Donate</h4>
-        <p>Please note that the smart contract is on a test node, meaning you will use goelri eth to vote, however donations are in main net eth.</p>
+        <p>
+          Please note that the smart contract is on a test node, meaning you
+          will use goelri eth to vote, however donations are in main net eth.
+        </p>
         <input type="text" placeholder="Amount" />
         <button>Submit</button>
       </div>
