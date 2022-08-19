7b6a5493fd85799d5d2c4c111f308b3a739d7f59
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-18 21:49:26 -0400

    added to donate section

--- src/components/donate/Donate.jsx
+++ src/components/donate/Donate.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './donate.css';
 
 export default function Donate() {
   return (
-    <div className='d-flex'>
+    <div className='donate-container'>
+      <div className="donate">
         <h4>If you enjoyoed my first Dapp, feel free to Donate</h4>
-        <input type="text" placeholder='Amount' />
+        <p>Please note that the smart contract is on a test node, meaning you will use goelri eth to vote, however donations are in main net eth.</p>
+        <input type="text" placeholder="Amount" />
+        <button>Submit</button>
+      </div>
     </div>
-  )
+  );
 }
